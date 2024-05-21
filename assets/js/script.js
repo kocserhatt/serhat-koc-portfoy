bbc4d118ec354f6ac7fbd754987cc86da05d3cad
serhat koç <kocserhat671@gmail.com>
2024-05-21 16:31:59 +0300

    düzenlemeler yapıldı

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navbarItems = document.querySelector('.items');
 
+hamburgerMenu.addEventListener('click', () => {
+    navbarItems.classList.toggle('show');
+});
+
+// Menü öğelerini gizlemek için
+document.addEventListener('click', (e) => {
+    if (!navbarItems.contains(e.target) && !hamburgerMenu.contains(e.target)) {
+        navbarItems.classList.remove('show');
+    }
+});
